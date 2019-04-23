4285b781588ddb46bcfbaee486264905f5c43c9f
31151020113 <32638405+NhuPhuongUEH@users.noreply.github.com>
2019-04-23 23:00:10 +0700

    HouseSpy_MVC_Core

---
+++ HouseSpy/HouseSpy/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouseSpy.Data;
+using HouseSpy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseSpy.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.ProductTypes.ToList());
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //create action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.ProductTypes.Add(productTypes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+    }
+}
---
+++ HouseSpy/HouseSpy/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HouseSpy.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseSpy.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
