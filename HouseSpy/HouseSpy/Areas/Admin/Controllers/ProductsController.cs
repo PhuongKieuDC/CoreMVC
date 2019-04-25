b3e1e485b820610406aa829b6ad7aba77fb19ab5
31151020113 <32638405+NhuPhuongUEH@users.noreply.github.com>
2019-04-25 23:15:42 +0700

    create, edit product

---
+++ HouseSpy/HouseSpy/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using HouseSpy.Data;
+using HouseSpy.Models.ViewModel;
+using HouseSpy.Utility;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseSpy.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly HostingEnvironment _hostingEnviroment;
+
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; }
+
+        public ProductsController(ApplicationDbContext context, HostingEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnviroment = hostingEnvironment;
+            ProductsVM = new ProductsViewModel()
+            {
+                ProductTypes = _context.ProductTypes.ToList(),
+                SpecialTags = _context.SpecialTags.ToList(),
+                Products = new Models.Products()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var product = _context.Products.Include(x => x.ProductTypes).Include(x => x.SpecialTag);
+            return View(await product.ToListAsync());
+        }
+
+        //get create action method
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+
+        //post create action method
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+            _context.Products.Add(ProductsVM.Products);
+            await _context.SaveChangesAsync();
+
+            //Image being saved
+            string webRootPath = _hostingEnviroment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            var productsFromDb = _context.Products.Find(ProductsVM.Products.Id);
+            if (files.Count != 0)
+            {
+                //image has been upload
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension;
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder + @"\" + SD.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg";
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //get edit action method
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _context.Products.Include(x => x.ProductTypes).Include(x => x.SpecialTag).Where(x => x.Id == id).SingleOrDefaultAsync();
+
+            if (ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductsVM);
+        }
+
+
+        //post edit action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostingEnviroment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                var productFromDb = _context.Products.Where(x => x.Id == ProductsVM.Products.Id).FirstOrDefault();
+
+                if (files.Count > 0 && files[0] != null)
+                {
+                    var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+
+                    var extension_new = Path.GetExtension(files[0].FileName);
+                    var extension_old = Path.GetExtension(productFromDb.Image);
+
+                    if (System.IO.File.Exists(Path.Combine(uploads, ProductsVM.Products.Id+extension_old)))
+                    {
+                        System.IO.File.Exists(Path.Combine(uploads, ProductsVM.Products.Id + extension_old));
+                    }
+                    using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension_new), FileMode.Create))
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+
+                    ProductsVM.Products.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension_new;
+                }
+
+                if (ProductsVM.Products.Image != null)
+                {
+                    productFromDb.Image = ProductsVM.Products.Image;
+                }
+
+                productFromDb.Name = ProductsVM.Products.Name;
+                productFromDb.Price = ProductsVM.Products.Price;
+                productFromDb.Available = ProductsVM.Products.Available;
+                productFromDb.ProductTypeId = ProductsVM.Products.ProductTypeId;
+                productFromDb.SpecialTagId = ProductsVM.Products.SpecialTagId;
+                productFromDb.ShadeColor = ProductsVM.Products.ShadeColor;
+                _context.Products.Update(productFromDb);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ProductsVM);
+        }
+    }
+}
