5240b275aa7f4332b48a4f760efeb098880f2b50
31151020113 <32638405+NhuPhuongUEH@users.noreply.github.com>
2019-05-09 15:00:14 +0700

    ajax

---
+++ CRUDJqueryAjax/CRUDJqueryAjax/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using CRUDJqueryAjax.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDJqueryAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        TodoApiEntities db = new TodoApiEntities();
+
+        public ActionResult Index()
+        {
+            List<Department> list = db.Departments.ToList();
+            ViewBag.ListofDepartment = new SelectList(list, "DepartmentId", "DepartmentName");
+            return View();
+        }
+
+        public JsonResult GetStudentList()
+        {
+            List<StudentViewModel> studentList = db.Students.Where(x => x.IsDeleted == false).Select(x => new StudentViewModel
+            {
+                StudentId = x.StudentId,
+                StudentName = x.StudentName,
+                Email = x.Email,
+                IsDeleted = x.IsDeleted,
+                DepartmentName = x.Department.DepartmentName
+            }).ToList();
+
+            return Json(studentList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetStudentById(int StudentId)
+        {
+            Student st = db.Students.Where(x => x.StudentId == StudentId).SingleOrDefault();
+            string value = string.Empty;
+            value = JsonConvert.SerializeObject(st, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+            return Json(value, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CRUDJqueryAjax/CRUDJqueryAjax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDJqueryAjax.Startup))]
+namespace CRUDJqueryAjax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
