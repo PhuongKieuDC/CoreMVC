8ab2907c38c6ac21abb4723d549626fd20bfd407
31151020113 <32638405+NhuPhuongUEH@users.noreply.github.com>
2019-05-15 23:21:12 +0700

    shoppingcartv2

--- HouseSpy/HouseSpy/Areas/Customer/Controllers/ShoppingCartController.cs
+++ HouseSpy/HouseSpy/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -42,5 +42,66 @@ namespace HouseSpy.Areas.Customer.Controllers
             }
             return View(ShoppingCartVM);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Index")]
+        public IActionResult IndexPost()
+        {
+            List<int> lstCartItem = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            ShoppingCartVM.Appointments.AppointmentDate = ShoppingCartVM.Appointments.AppointmentDate.AddHours(ShoppingCartVM.Appointments.AppointmentTime.Hour).AddMinutes(ShoppingCartVM.Appointments.AppointmentTime.Minute);
+            Appointments appointments = ShoppingCartVM.Appointments;
+            _context.Appointments.Add(appointments);
+            _context.SaveChanges();
+
+            int apponitmentId = appointments.Id;
+
+            foreach(int productId in lstCartItem)
+            {
+                ProductsSelectedForAppointment productsSelectedForAppointment = new ProductsSelectedForAppointment()
+                {
+                    AppointmentId = apponitmentId,
+                    ProductId = productId
+                };
+                _context.ProductsSelectedForAppointments.Add(productsSelectedForAppointment);
+                _context.SaveChanges();
+            }
+
+            _context.SaveChanges();
+
+            //set bag null when click button submit
+            lstCartItem = new List<int>();
+            HttpContext.Session.Set("ssShoppingCart", lstCartItem);
+            return RedirectToAction("AppointmentConfirm", "ShoppingCart", new { Id = apponitmentId});
+        }
+
+        public IActionResult remove(int id)
+        {
+            List<int> lstCartItems = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if (lstCartItems.Count > 0)
+            {
+                if(lstCartItems.Contains(id))
+                {
+                    lstCartItems.Remove(id);
+                }
+            }
+
+            HttpContext.Session.Set("ssShoppingCart", lstCartItems);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult AppointmentConfirm(int id)
+        {
+            ShoppingCartVM.Appointments = _context.Appointments.Where(x => x.Id == id).FirstOrDefault();
+            List<ProductsSelectedForAppointment> objProducts = _context.ProductsSelectedForAppointments.Where(x => x.AppointmentId == id).ToList();
+            
+            foreach (ProductsSelectedForAppointment obj in objProducts)
+            {
+                ShoppingCartVM.Products.Add(_context.Products.Include(x => x.ProductTypes).Include(x => x.SpecialTag).Where(x => x.Id == obj.ProductId).FirstOrDefault());
+            }
+
+            return View(ShoppingCartVM);
+        }
     }
 }
