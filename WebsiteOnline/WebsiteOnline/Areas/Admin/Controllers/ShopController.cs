b58bf17e1c90b091b090a86aee83b3837583fb4b
31151020113 <32638405+NhuPhuongUEH@users.noreply.github.com>
2019-05-21 22:34:56 +0700

    jquery add new category

---
+++ WebsiteOnline/WebsiteOnline/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteOnline.Models.Data;
+
+namespace WebsiteOnline.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        ProjectReviewEntities db = new ProjectReviewEntities();
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+            List<DanhMuc> list = db.DanhMucs.ToArray().OrderBy(x => x.Sort).ToList();
+            return View(list);
+        }
+
+        [HttpPost]
+        public string AddNewCategory(string catname)
+        {
+            string id;
+
+            if (db.DanhMucs.Any(x => x.Ten == catname))
+                return "titletaken";
+            DanhMuc danhMuc = new DanhMuc();
+            danhMuc.Ten = catname;
+            danhMuc.Slug = catname.Replace(" ", "-").ToLower();
+            danhMuc.Sort = 100;
+
+            db.DanhMucs.Add(danhMuc);
+            db.SaveChanges();
+
+            id = danhMuc.MaDanhMuc.ToString();
+
+            return id;
+        }
+    }
+}
